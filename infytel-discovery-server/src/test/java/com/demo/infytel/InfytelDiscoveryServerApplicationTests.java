74a7954afacc9f9642405ed84fc7717334ff84d0
MirAbbasAli <abbasalimir97@gmail.com>
2022-05-18 22:43:55 +0530

    add pom.xml for infytel-discovery-server

---
+++ infytel-discovery-server/src/test/java/com/demo/infytel/InfytelDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.infytel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfytelDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
