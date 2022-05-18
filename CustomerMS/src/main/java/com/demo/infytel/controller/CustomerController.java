07740fdfee66cad2d440c1d5b349769008dd4439
MirAbbasAli <abbasalimir97@gmail.com>
2022-05-18 23:13:12 +0530

    Discovering services through Eureka

--- CustomerMS/src/main/java/com/demo/infytel/controller/CustomerController.java
+++ CustomerMS/src/main/java/com/demo/infytel/controller/CustomerController.java
@@ -1,13 +1,14 @@
 package com.demo.infytel.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.demo.infytel.LoadBalancerConfig;
 import com.demo.infytel.dto.CustomerDTO;
 import com.demo.infytel.dto.LoginDTO;
 import com.demo.infytel.dto.PlanDTO;
@@ -26,19 +26,23 @@ import com.demo.infytel.service.CustomerService;
 @RefreshScope
 @RestController
 @CrossOrigin
-@LoadBalancerClient(name="MyLoadBalancer", configuration=LoadBalancerConfig.class)
+//@LoadBalancerClient(name="MyLoadBalancer", configuration=LoadBalancerConfig.class)
 public class CustomerController {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private CustomerService custService;
 	
-	@Autowired
-	private RestTemplate template;
-	
-	@Value("${plan.uri}")
-	private String planUri;
+	/*
+	 * @Autowired private RestTemplate template;
+	 * 
+	 * @Value("${plan.uri}") private String planUri;
+	 */
 
+	@Autowired
+	private DiscoveryClient client;
+	
+	
 	// Create a new customer
 	@RequestMapping(value = "/customers", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void createCustomer(@RequestBody CustomerDTO custDTO) {
@@ -58,11 +62,26 @@ public class CustomerController {
 	public CustomerDTO getCustomerProfile(@PathVariable Long phoneNo) {
 
 		logger.info("Profile request for customer {}", phoneNo);
+		
+		List<ServiceInstance> friendInstances=client.getInstances("FRIENDFAMILYMS");
+		ServiceInstance friendInstance=friendInstances.get(0);
+		URI friendUri=friendInstance.getUri();
+		
+		List<ServiceInstance> planInstances=client.getInstances("PLANMS");
+		ServiceInstance planInstance=planInstances.get(0);
+		URI planUri=planInstance.getUri();
+		
 		CustomerDTO custDTO=custService.getCustomerProfile(phoneNo);
 		// Add rest endpoint to replace foreign key contraint for plan table
-		PlanDTO planDTO=new RestTemplate().getForObject(planUri+custDTO.getCurrentPlan().getPlanId(), PlanDTO.class);
+//		PlanDTO planDTO=new RestTemplate().getForObject(planUri+custDTO.getCurrentPlan().getPlanId(), PlanDTO.class);
+		
+		
+		PlanDTO planDTO=new RestTemplate().getForObject(planUri+"/plans/"+custDTO.getCurrentPlan().getPlanId(), PlanDTO.class);
 		custDTO.setCurrentPlan(planDTO);
-		List<Long> friends=template.getForObject("http://MyLoadBalancer/customers/"+phoneNo+"/friends", List.class);
+
+		@SuppressWarnings("unchecked")
+		//		List<Long> friends=template.getForObject("http://MyLoadBalancer/customers/"+phoneNo+"/friends", List.class);
+		List<Long> friends=new RestTemplate().getForObject(friendUri+"/customers/"+phoneNo+"/friends", List.class);
 		custDTO.setFriendAndFamily(friends);
 		return custDTO;
 	}
