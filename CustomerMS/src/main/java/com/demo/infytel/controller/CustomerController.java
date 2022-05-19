14f136ddb2de100344c227d612cd06e063fd4f53
abbas <emperorabbas@gmail.com>
2022-05-20 00:21:18 +0530

    removed unused imports from the files

--- CustomerMS/src/main/java/com/demo/infytel/controller/CustomerController.java
+++ CustomerMS/src/main/java/com/demo/infytel/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.demo.infytel.controller;
 
 import java.util.List;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
