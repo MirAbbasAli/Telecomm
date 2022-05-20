d36b71e027eae5fbe136e30f1a0007eb17fa27c4
MirAbbasAli <abbasalimir97@gmail.com>
2022-05-20 21:46:52 +0530

    Add other files required for the gateway

---
+++ Gateway/src/test/java/com/demo/infytel/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.infytel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
